31428d2cb4d5ca3b1cf7165a4317c658a3f87560
chief666 <787579223@qq.com>
2019-02-28 11:04:14 +0800

    helloworld

---
+++ src/main/java/com/soft1721/spring/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.soft1721.spring.hello;
+
+public class HelloWorld {
+    public static String getHello(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/soft1721/spring/hello/StudentAPP.java
@@ -0,0 +1,12 @@
+package com.soft1721.spring.hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StudentAPP {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
+}
